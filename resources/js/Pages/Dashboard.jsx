403d4606bc6e3ba06f171fbf5569f5f66345f041
IRU212 <ryuuya2121b>
2022-09-26 13:52:00 +0900

    header search cart home design

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -4,11 +4,14 @@ import { Head } from '@inertiajs/inertia-react';
 
 export default function Dashboard(props) {
     return (
-        <AuthenticatedLayout
-            auth={props.auth}
-            errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-        </AuthenticatedLayout>
+        // <AuthenticatedLayout
+        //     auth={props.auth}
+        //     errors={props.errors}
+        //     header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        // >
+        // </AuthenticatedLayout>
+        <div>
+            aaaa
+        </div>
     );
 }
--- resources/js/Pages/Header.jsx
+++ resources/js/Pages/Header.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
+import { useState } from 'react';
+import styled from 'styled-components'
+
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
+
+    const HeaderDiv = styled.div`
+        width: auto;
+        height: 60px;
+        margin: 0;
+        padding: 0 7%;
+        line-height: 60px;
+        background-color: #565053;
+        color: #fff;
+        font-size: 1.1rem;
+        display: flex;
+        position: relative;
+    `;
+
+    const FormPosition = styled.form`
+        position: absolute;
+        top: 0%;
+        left: 50%;
+        transform: translate(-50%,0%);
+    `
+
+    const SearchInput = styled.input`
+        width: 600px;
+        height: 38px;
+        padding: 5px 0 5px 36px;
+        border-radius: 5px;
+        color: #000;
+    `
+    const ButtonImg = styled.img`
+        position: relative;
+        top: 6px;
+        left: 31px;
+    `
+
+    const ALinkDiv = styled.a`
+        
+    `
+
+    const CartIconDiv = styled.div`
+        position: absolute;
+        right: 8%;
+        font-size: 1.3rem;
+
+        &:hover{
+            cursor: pointer;
+        }
+    `
+
+    const [ text,setText ] = useState()
+
+    const handleTextChange = (e) => {
+        setText(e.target.value)
+    }
+
     return (
         <div>
-            Header
+            <HeaderDiv>
+                <ALinkDiv href='http://localhost:8000'>
+                    Home
+                </ALinkDiv>
+                <FormPosition>
+                    <button><ButtonImg width={24} height={24} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTliEHR87W0oPjpBMR6U9HkbxrOJqBq0WGNoqIynxY&s" alt="" /></button>
+                    <SearchInput type="text" value={text} onChange={handleTextChange} />
+                </FormPosition>
+                <CartIconDiv>
+                    <ShoppingCartIcon />
+                </CartIconDiv>
+            </HeaderDiv>
         </div>
     )
 }

3e7838ee533bdb0bb92fee147715ed9713f47744
IRU212 <ryuuya2121b>
2022-09-27 11:20:50 +0900

    chat listDetail moreButton Toggle

--- resources/js/Pages/Chat.jsx
+++ resources/js/Pages/Chat.jsx
@@ -5,6 +5,7 @@ import styles from '../../css/content.module.scss'
 function Chat(props) {
 
     const [datas,setData] = useState()
+    const [moreButtonDisplay,setMoreButtonDisplay] = useState(true)
 
     useEffect(() => {
         axios
@@ -18,29 +19,65 @@ function Chat(props) {
             })
     },[])
 
-    console.log(props)
-
     const results = datas?.filter( data => {
         return data.product_id == props.info.dataProductId
     } )
 
+    const ClickMoreButtonToggle = () => {
+        setMoreButtonDisplay(!moreButtonDisplay)
+    }
+
     return (
-        <div>
-            { results?.map((data,index) => {
-                if (data.user_id == props.info.dataUserId) {
-                    return(
-                        <div key={index}>
-                            true
-                        </div>
-                    )
-                } else {
-                    return(
-                        <div key={index}>
-                            false
-                        </div>
-                    )
-                }
-            }) }
+        <div className={styles.Chat}>
+            <details>
+                <summary className={styles.details}>
+                    { results?.map((data,index) => {
+                        if ( index < 3) {
+                            if (data.user_id == props.info.dataUserId) {
+                                return(
+                                    <div className={styles.ChatWidthTrue}>
+                                        <div key={index} className={styles.ChatTrue}>
+                                            { data.text }
+                                        </div>
+                                    </div>
+                                )
+                            } else {
+                                return(
+                                    <div className={styles.ChatWidthFalse}>
+                                        <div key={index} className={styles.ChatFalse}>
+                                            { data.text }
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        }
+                    }) }
+                    <div className={`${moreButtonDisplay ? styles.moreButtonBlock : styles.moreButtonNone}`} onClick={ClickMoreButtonToggle}>
+                        もっと見る
+                    </div>
+                </summary>
+                { results?.map((data,index) => {
+                        if ( index > 3) {
+                            if (data.user_id == props.info.dataUserId) {
+                                return(
+                                    <div className={styles.ChatWidthTrue}>
+                                        <div key={index} className={styles.ChatTrue}>
+                                            { data.text }
+                                        </div>
+                                    </div>
+                                )
+                            } else {
+                                return(
+                                    <div className={styles.ChatWidthFalse}>
+                                        <div key={index} className={styles.ChatFalse}>
+                                            { data.text }
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        }
+                    }) }
+            </details>
     </div>
     )
 }

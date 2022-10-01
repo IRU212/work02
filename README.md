f3d10eec3bb944a0ef0ce8ad3044db967ac7872c
IRU212 <ryuuyapro@gmail.com>
2022-10-01 15:09:50 +0900

    table cart_details 作成 carts 変更

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ##### npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
 ##### npm iinstal axios
 ##### npm install  react-hooks-use-modal
+
+##### composer require doctrine/dbal
---
+++ database/migrations/2022_10_01_055046_create_cart_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('money')->comment('商品合計金額');
+            $table->integer('quantity')->comment('個数');
+            $table->string('product_id');
+            $table->string('cart_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_details');
+    }
+};

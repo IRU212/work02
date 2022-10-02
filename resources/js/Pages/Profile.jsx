a7bd69eb532065b6fae527017a1d139709b55474
IRU212 <ryuuyapro@gmail.com>
2022-10-02 17:05:20 +0900

    profile.jsx ユーザ情報　表示

--- resources/js/Pages/Profile.jsx
+++ resources/js/Pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from '../../css/profile.module.scss'
 import Header from './Header'
+import ProductItemList from './ProductItemList'
+import ProductList from './ProductList'
 
 function Profile(props) {
     return (
@@ -8,6 +10,19 @@ function Profile(props) {
             <Header
                 info={ props.auth.user }
             />
+            <div className={styles.backImg}>
+                    <img src="https://www.helloimg.com/images/2021/12/21/G0OMtS.jpg" alt="" />
+                </div>
+            <div className={styles.intoroduce}>
+                <div className={styles.IconImg}>
+                    <img src={`http://localhost:8000/${props.auth.user.image}`} alt="" />
+                </div>
+                <div className={styles.profileText}>
+                    <div className={styles.profileName}>
+                        aaa
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ resources/js/Pages/ProfileProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfileProductList() {
+    return (
+        <div>
+            ProfileProductList
+        </div>
+    )
+}
+
+export default ProfileProductList
